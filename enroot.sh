3b3893517b1867be4033bb4fd910988e181c40fc
Nagarajan Karthik <nkarthik3092@gmail.com>
2025-12-18 17:02:52 +0800

    Added script to set enroot environment variables.

---
+++ enroot.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export ENROOT_CACHE_PATH=/scratch/Projects/SPEC-SF-AISG/enroot/cache
+export ENROOT_TEMP_PATH=/scratch/Projects/SPEC-SF-AISG/enroot/tmp
+export ENROOT_DATA_PATH=/scratch/Projects/SPEC-SF-AISG/enroot/data
+export ENROOT_RUNTIME_PATH=/scratch/Projects/SPEC-SF-AISG/enroot/runtime
