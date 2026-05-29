7566f7c81d1902cf05cae0778f69ee1a75f7d85d
Nagarajan Karthik <nkarthik3092@gmail.com>
2026-05-29 11:55:31 +0800

    Deleted old versions of files.

--- enroot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export ENROOT_CACHE_PATH=/scratch/Projects/SPEC-SF-AISG/enroot/cache
-export ENROOT_TEMP_PATH=/scratch/Projects/SPEC-SF-AISG/enroot/tmp
-export ENROOT_DATA_PATH=/scratch/Projects/SPEC-SF-AISG/enroot/data
-export ENROOT_RUNTIME_PATH=/scratch/Projects/SPEC-SF-AISG/enroot/runtime
